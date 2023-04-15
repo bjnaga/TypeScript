dceae853f2ad32f33b30f159d7ded95ac8813f12
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-15 19:40:39 +0530

    added exemplify code, and all the codes were added to a new folder named defn and sample code

---
+++ Defn_and_samplecode_creation/Typescript.ts
@@ -0,0 +1,4 @@
+let grade = 95; // its type infering the values
+let grade1 = true; // here it automatically consideres boolean
+// annotating the code would be the best way to go forward as shown below
+let grade3: number = 95;
