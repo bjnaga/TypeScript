932cdbc87f13d2fd3f7c4b5b2a1c13bfb1f8331a
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 13:26:48 +0530

    created a function expression file . updated sample function expression code in the file

--- dist/4.functions/function-expression.js
+++ dist/4.functions/function-expression.js
@@ -1,1 +1,6 @@
 "use strict";
+let x = function (x, y, z) {
+    let a = 10;
+    return a;
+};
+console.log(x);
---
+++ src/4.functions/function-expression.ts
@@ -0,0 +1,6 @@
+// a simple function expression would look like
+let x = function (x: number, y: number, z: string): number {
+  let a: number = 10;
+  return a;
+};
+console.log(x);
