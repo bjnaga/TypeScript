59db24d6cd5fd20e17a457d1e94a37df0a686192
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 20:42:09 +0530

    demo of different type of literal type usage

--- dist/5.TS_basic_types/literal_type.js
+++ dist/5.TS_basic_types/literal_type.js
@@ -1,1 +1,12 @@
 "use strict";
+let numberILike = 45;
+let numberILike1 = 34;
+let myString = "Holiday";
+let myString1 = "Holiday1";
+let numberILike2 = 34;
+let tipp = (billAmount, tipPercentage) => {
+    return (billAmount * tipPercentage) / 100;
+};
+let tipAmountt = tipp(100, 10);
+console.log(tipAmountt);
+let frontendPathInclude = "HTML";
--- src/5.TS_basic_types/literal_type.ts
+++ src/5.TS_basic_types/literal_type.ts
@@ -1,1 +1,24 @@
 //restricts a variable or a function parameters can accept
+let numberILike: 45 = 45; // it can only value 45 and nothing else
+let numberILike1: 34 | 35 = 34; // can only accept 34 or 35
+
+let myString: "Holiday" = "Holiday";
+let myString1: "Holiday1" | "Holiday2" = "Holiday1";
+
+//  create your own type myNewType to accept only 5 number mentioned below
+type myNewType = 45 | 34 | 46 | 33 | 98;
+let numberILike2: myNewType = 34;
+
+//
+
+let tipp = (billAmount: number, tipPercentage: 10 | 20 | 30): number => {
+  return (billAmount * tipPercentage) / 100;
+};
+
+let tipAmountt = tipp(100, 10);
+console.log(tipAmountt);
+
+//-------------------------------------------
+type frontEnd = "HTML" | "CSS" | "JavaScript" | "React";
+
+let frontendPathInclude: frontEnd = "HTML";
