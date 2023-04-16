44506dc95d1997217f9377adc15cb2ad072564e4
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 11:05:17 +0530

    adding if branching demo file

---
+++ src/branch_and_loop/if-stmt.ts
@@ -0,0 +1,9 @@
+let num1: number = 10;
+let num2: number = 5;
+let num3: number = 15;
+
+if (num1 > num2) {
+  console.log(`${num1} is greater than ${num2}`);
+} else if (num1 < num2) {
+  console.log(`${num1} is greater than ${num2}`);
+} else console.log(`${num1} and ${num2} are equal`);
