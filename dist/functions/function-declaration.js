0753f021cc3d1fb431d35a36f85f508467f306e3
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 12:49:21 +0530

    demonstrating function with return type and function without return type

--- dist/functions/function-declaration.js
+++ dist/functions/function-declaration.js
@@ -1,1 +1,6 @@
 "use strict";
+function holidaySale(product, sales, tax, discount) { }
+function holidaySale1(product, sales, tax, discount) {
+    let x = 10;
+    return x;
+}
---
+++ src/functions/function-declaration.ts
@@ -0,0 +1,18 @@
+// functions are created using the keyword function and the below code has void return type as the return type is not mentioned in the below code
+function holidaySale(
+  product: string,
+  sales: number,
+  tax: number,
+  discount: number
+) {}
+
+// this below code contains return type unlike previous function.
+function holidaySale1(
+  product: string,
+  sales: number,
+  tax: number,
+  discount: number
+): number {
+  let x: number = 10;
+  return x;
+}
