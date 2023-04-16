21cd8d1a28efd5345204647574a8a92e58d2407f
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 15:43:59 +0530

    added type alias code dome

--- dist/5.TS_basic_types/type_aliases.js
+++ dist/5.TS_basic_types/type_aliases.js
@@ -1,3 +1,5 @@
 "use strict";
 let currentDiscount = 15;
 currentDiscount = 5;
+let customerDiscount = 10;
+let customerDiscountThisSeason = 10;
--- src/5.TS_basic_types/type_aliases.ts
+++ src/5.TS_basic_types/type_aliases.ts
@@ -2,3 +2,11 @@
 type CurrentDiscount = number;
 let currentDiscount: CurrentDiscount = 15;
 currentDiscount = 5;
+
+// literal type for variable
+//  all this will do it make sure the value for customerDiscount will be one of the values and not something else other than 10 | 20 | 30 | 40
+let customerDiscount: 10 | 20 | 30 | 40 = 10;
+
+// alternative easier code creation would be something like this
+type CustomerDiscount = 10 | 20 | 30 | 40;
+let customerDiscountThisSeason: CurrentDiscount = 10;
