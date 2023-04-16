76be4444f57da6a8f46b72dbe0e2a05435c33909
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 15:39:53 +0530

    type alias file created with sample alias demo

---
+++ dist/5.TS_basic_types/type_aliases.js
@@ -0,0 +1,3 @@
+"use strict";
+let currentDiscount = 15;
+currentDiscount = 5;
---
+++ src/5.TS_basic_types/type_aliases.ts
@@ -0,0 +1,4 @@
+// type is used to create alternative names for the type number string boolean etc.
+type CurrentDiscount = number;
+let currentDiscount: CurrentDiscount = 15;
+currentDiscount = 5;
