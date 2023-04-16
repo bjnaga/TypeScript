5cefcaa3e943a1aa7cb489dc7e55e0d3ae190d2f
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 17:46:00 +0530

    recursive type with example demo for numbers are created here

--- dist/5.TS_basic_types/recursive_type.js
+++ dist/5.TS_basic_types/recursive_type.js
@@ -1,1 +1,5 @@
 "use strict";
+const numbers1 = [1, [1, 3]];
+const numbers2 = [1, [3]];
+const numbers3 = [1, [3, [4, 5, 5, 6]]];
+const numbers4 = [1, [3, [4, 5, 5, 6, [23, 456, 567]]]];
--- src/5.TS_basic_types/recursive_type.ts
+++ src/5.TS_basic_types/recursive_type.ts
@@ -1,2 +1,13 @@
 // recursive type is a type that references itself
-//
+// eg an array of nested number and nested array
+
+type NestedArray = number | NestedArray[];
+//  type is resolved during compilation and no traces of it will ever exist during runtime
+
+const numbers1: NestedArray = [1, [1, 3]];
+
+const numbers2: NestedArray = [1, [3]];
+
+const numbers3: NestedArray = [1, [3, [4, 5, 5, 6]]];
+
+const numbers4: NestedArray = [1, [3, [4, 5, 5, 6, [23, 456, 567]]]];
