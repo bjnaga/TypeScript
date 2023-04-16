bc52f8ef1fa24981db946d03c8a2c03cb54f3d72
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-16 11:14:20 +0530

    array of numbers were created and contents of the are displayed using for loop

--- dist/branch_and_loop/for-loop.js
+++ dist/branch_and_loop/for-loop.js
@@ -1,3 +1,6 @@
 "use strict";
 for (let i = 0; i < 10; i++)
     console.log(i);
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < nums.length; i++)
+    console.log(nums[i]);
--- src/branch_and_loop/for-loop.ts
+++ src/branch_and_loop/for-loop.ts
@@ -1,1 +1,5 @@
 for (let i = 0; i < 10; i++) console.log(i);
+
+// another for loop variation
+let nums: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i: number = 0; i < nums.length; i++) console.log(nums[i]);
