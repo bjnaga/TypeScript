bb08dd97076940159f2ce64e57e2d81f1e029882
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-17 19:41:49 +0530

    interesection demo added

--- dist/5.TS_basic_types/intersection_type.js
+++ dist/5.TS_basic_types/intersection_type.js
@@ -1,1 +1,13 @@
 "use strict";
+const alanWrench = {
+    id: 1,
+    category: "hardware",
+    name: "Alan Wrench",
+    price: 12.99,
+    stock: 0,
+    discount() { },
+    holidaySales: () => { },
+    subscription() { },
+    shipping() { },
+};
+console.log(alanWrench);
---
+++ src/5.TS_basic_types/intersection_type.ts
@@ -0,0 +1,41 @@
+type ProductProperties = {
+  id: number;
+  category: string;
+  name: string;
+  price: number;
+  stock: number;
+};
+
+type ProductDiscount = {
+  discount(): void;
+};
+type ProductSeasonalSales = {
+  holidaySales(): void;
+};
+type ProductSubscription = {
+  subscription(): void;
+};
+type ProductShipping = {
+  shipping(): void;
+};
+
+// in union type either this or that
+// but with intersection type its this and that , and is denoted by "&"
+type Product = ProductProperties &
+  ProductDiscount &
+  ProductSeasonalSales &
+  ProductSubscription &
+  ProductShipping;
+
+const alanWrench: Product = {
+  id: 1,
+  category: "hardware",
+  name: "Alan Wrench",
+  price: 12.99,
+  stock: 0,
+  discount() {},
+  holidaySales: () => {},
+  subscription() {},
+  shipping() {},
+};
+console.log(alanWrench);
