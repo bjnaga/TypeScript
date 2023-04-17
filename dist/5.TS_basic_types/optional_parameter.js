c17468c981066bcf6d01b2a15bbfed15ccfa21d5
Naga Ganesh B J <bjnaga@gmail.com>
2023-04-17 20:32:15 +0530

    optional parameter with default type is demonstrated in the same example

--- dist/5.TS_basic_types/optional_parameter.js
+++ dist/5.TS_basic_types/optional_parameter.js
@@ -1,6 +1,6 @@
 "use strict";
 let productName2;
-productName2 = (product, price) => {
+productName2 = (product, price = 10) => {
     return `${product},${price}`;
 };
 console.log(productName2("TSExercise"));
--- src/5.TS_basic_types/optional_parameter.ts
+++ src/5.TS_basic_types/optional_parameter.ts
@@ -1,8 +1,10 @@
 // parameters whose values are not necessarily required, by adding "?" we are able to make this as an option
 // if we don't provide the price the ts while transforming wont generate an error
-let productName2: (product: string, price?: number) => string;
-
-productName2 = (product: string, price?: number) => {
+let productName2: (product: string, price?: number) => string; // default values cant be used in signature of function it should be instead used with function definition
+// default values must be only present within a function definition or constructor
+//  without optional parameter the we have to use "?" in price?: number
+// productName2 = (product: string, price?: number ) => {
+productName2 = (product: string, price: number = 10) => {
   //providing default parameter value would solve the error associated with printing undefined printing
   return `${product},${price}`;
 };
